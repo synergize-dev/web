40a1659e4b7e9f370af9eebd3d9b36c9ec452195
p4u <pau@dabax.net>
2024-12-31 11:03:02 +0100

    Remove mouse follower effect

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,11 @@ import HeroSection from './components/HeroSection.vue'
 import ServicesSection from './components/ServicesSection.vue'
 import InvestmentsSection from './components/InvestmentsSection.vue'
 import ContactSection from './components/ContactSection.vue'
-import MouseFollower from './components/MouseFollower.vue'
 import SectionSeparator from './components/layout/SectionSeparator.vue'
 </script>
 
 <template>
   <div class="min-h-screen bg-black">
-    <MouseFollower />
     <TheNavbar />
     <main class="bg-black">
       <HeroSection />
@@ -25,16 +23,6 @@ import SectionSeparator from './components/layout/SectionSeparator.vue'
 </template>
 
 <style>
-/* Hide default cursor */
-* {
-  cursor: none;
-}
-
-/* Show cursor on interactive elements */
-a, button, input, textarea, select {
-  cursor: pointer !important;
-}
-
 /* Smooth scrolling */
 html {
   scroll-behavior: smooth;
@@ -45,9 +33,4 @@ body {
   background-color: black;
 }
 
-/* Ensure proper stacking */
-.cursor {
-  position: fixed !important;
-  z-index: 999999 !important;
-}
 </style>
